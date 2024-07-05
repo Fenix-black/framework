25a8da751a16f479e8235db279335d20d8f9d6d9
Pablo Schaffner <pablo@puntorigen.com>
2024-07-05 11:28:53 -0400

    - removed some unused files

--- web/src/types/declarations.d.ts
+++ web/src/types/declarations.d.ts
@@ -1,9 +1,4 @@
 // src/types/declarations.d.ts
-
-declare module 'clippyjs' {
-    var exportAsAny: any;
-    export default exportAsAny;
-}
 declare module '@nice-avatar-svg/react' {
     var exportAsAny: any;
     export default exportAsAny;
