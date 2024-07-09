2392c84e6cd636d701780e181e2091087a0d6db8
Pablo Schaffner <pablo@puntorigen.com>
2024-07-09 09:04:33 -0400

    - improved linkedin expert

--- web/src/experts/LinkedinExpert.jsx
+++ web/src/experts/LinkedinExpert.jsx
@@ -26,6 +26,14 @@ const LinkedinExpert = forwardRef(({
         # For example, to search for a person named 'John Doe' you can use the search engine and type 'site:linkedin.com "John Doe"' and then get the link to the person's profile.
         # Also you know that the best way to get the information for a Linkedin profile is to use a prefix like 'archive.md/linkedin-link' (e.g. archive.md/http://linkedin.com/in/johndoe) to get the information from the profile.
         # The other option you have is to simulate being a mobile browser and use the Linkedin mobile website to get the information you need.
+        
+        # If you need to get information about a certain role instead of a person, you can use the search engine and type 'site:linkedin.com "role"' and then get the links to the people with that role.
+        # If you need to get information about a certain role and from a certain industry, you can use the search engine and type 'site:linkedin.com "role" "industry"' and then get the links to the people with that role in that industry.
+        # If you need to get information about a certain role and from a certain company, you can use the search engine and type 'site:linkedin.com "role" "company"' and then get the links to the people with that role in that company.
+        # Also you can specify the country as well by using 'site:linkedin.com "role" "company" "country"'. Ensure to use double quotes to specify the search terms, so we get exact matches.
+        # For example, to search for people with the role "confiabilidad de activos" in "Chile", you would search for 'site:linkedin.com "confiabilidad de activos" "Chile"'.
+
+        # After you get the webpage of the profiles, you need to be able to extract the information from the page, for which you can use any tool available to you.
         `,
         // how the experts talks back to the user like in the meeting
         personality: `Always refer to your actions as first person and never use exagerated words, and be conscise on the texts you are saying you're going to perform. Instead of saying you will perform them in the future, say you are doing them now.`, 
